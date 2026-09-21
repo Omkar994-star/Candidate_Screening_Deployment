319564e16b35770b579350f71e1db5fceab08c3a
Omkar994-star <jadhavomkar3110@gmail.com>
2026-09-21 21:03:09 +0530

    Updated frontend path for cors

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://keen-amazement-production-4f9b.up.railway.app",
+        "https://ai-candidate-screening.up.railway.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
